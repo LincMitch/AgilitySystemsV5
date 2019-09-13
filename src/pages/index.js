c426ec34fa97d68be11d1d54975368e851906eca
Lincoln Mitchell <lincoln@identitii.com>
2019-09-13 20:38:27 +1000

    pushing everything upto Interactions and Animations course

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,13 +8,13 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
+    <div className="Hero">
+      <div className="HeroGroup">
+        <h1>AGILITY SYSTEMS</h1>
+        <p>Wherever you are in your journey, we're here to help</p>
+        <Link to="/page-2/">Go to page 2</Link>        
+      </div>
     </div>
-    <Link to="/page-2/">Go to page 2</Link>
   </Layout>
 )
 
